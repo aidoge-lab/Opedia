38a87c4b25d30289a8b49031fb0ca3d2a42c29c1
ai-doge <123085890+ai-doge@users.noreply.github.com>
2025-01-25 11:23:34 +0800

    add gpt2

--- download_model.py
+++ download_model.py
@@ -2,9 +2,16 @@ from transformers import AutoImageProcessor, ResNetForImageClassification
 import torch
 
 # output model structure (resnet50)
-print("ResNetForImageClassification")
-model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
-with open('./models/cv/resnet50/model.structure', 'w') as f:
+# print("ResNetForImageClassification")
+# model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+# with open('./models/cv/resnet50/model.structure', 'w') as f:
+#     print(model, file=f)
+
+# output model structure (gpt2)
+print("GPT2Model")
+from transformers import GPT2Tokenizer, GPT2Model
+model = GPT2Model.from_pretrained('gpt2')
+with open('./models/nlp/gpt2/model.structure', 'w') as f:
     print(model, file=f)
 
 
